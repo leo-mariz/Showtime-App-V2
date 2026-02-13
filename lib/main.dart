25bb685a396ac4da978e4bf91633ecc344b78d1d
Leo Ammon <leoammon@Leos-MacBook-Air.local>
2026-02-13 10:28:41 -0300

    Finalizacao do app

--- lib/main.dart
+++ lib/main.dart
@@ -1119,27 +1119,24 @@ Future <void> main() async {
     // 1. Debug provider
     // 2. Safety Net provider
     // 3. Play Integrity provider
-    androidProvider: AndroidProvider.debug,
+    androidProvider: AndroidProvider.playIntegrity,
     // Default provider for iOS/macOS is the Device Check provider. You can use the "AppleProvider" enum to choose
         // your preferred provider. Choose from:
         // 1. Debug provider
         // 2. Device Check provider
         // 3. App Attest provider
          // 4. App Attest provider with fallback to Device Check provider (App Attest provider is only available on iOS 14.0+, macOS 14.0+)
-     appleProvider: AppleProvider.debug, // Use debug para obter token de debug no Xcode
+     appleProvider: AppleProvider.appAttest, // Use debug para obter token de debug no Xcode
   );
   
   await AutoCacheInitializer.initialize(
     configuration: CacheConfiguration(
       sizeOptions: CacheSizeOptions(
-        
       ),
       // cryptographyOptions: CacheCryptographyOptions(),
       dataCacheOptions: DataCacheOptions(
-
       )
     ),
-
   );
   setupLocator();
   
